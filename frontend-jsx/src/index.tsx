487b670124e92acf8c182d56793309fc2de2b785
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-11-05 12:43:49 -0400

    fix: show default page

--- frontend-jsx/src/index.tsx
+++ frontend-jsx/src/index.tsx
@@ -1,15 +1,13 @@
-import Navbar from "./components/Navbar";
 import MainContainer from "./components/MainContainer";
+import Navbar from "./components/Navbar";
 import Enact from "./Enact";
-import ReservationModal from "./components/ReservationModal";
 
 
 function Root() {
   return (
     <div>
-      <ReservationModal start_date="11:00AM" end_date="12:00PM" building="ILC" room = "ILC151" />
-
-      
+      <Navbar />
+      <MainContainer />
     </div>
   );
 }
