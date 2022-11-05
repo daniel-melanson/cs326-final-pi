854fb855c11807327f6199a44bdeeb396bf2cc5b
anthony <anthony_zalev@hotmail.com>
2022-11-05 15:27:15 -0400

    basic stuff

--- frontend-jsx/src/components/ReservationModal.tsx
+++ frontend-jsx/src/components/ReservationModal.tsx
@@ -28,15 +28,20 @@ function closeModal(id: string){
     myModal.dispose();
 }
 
+function confirmEvent(id : string){
+    //do stuff
+
+    closeModal(id);
+}
 export default function ReservationModal(props : DropDownButtonProps){
-    let uniqueid = props.room + props.building + props.start_date + props.end_date;
+    let uniqueid = JSON.stringify(props);
     setTimeout( () => triggerModal(uniqueid), 100);
     //setTimeout( () => closeModal(uniqueid), 2000);
     let closeButtonId = uniqueid +'closeButton';
     setTimeout( () => document.getElementById(closeButtonId)?.addEventListener('click', (() => closeModal(uniqueid))), 200);
    
     return(
-        <div class="modal fade" id={props.room + props.building + props.start_date + props.end_date} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id={uniqueid} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                 <div class="modal-header">
@@ -49,16 +54,16 @@ export default function ReservationModal(props : DropDownButtonProps){
                     <form>
                     <div class="form-group">
                         <label for="recipient-name" class="col-form-label">Event Title:</label>
-                        <input type="text" class="form-control" id="recipient-name"></input>
+                        <input type="text" class="form-control" id={uniqueid +"event-name"}></input>
                     </div>
                     <div class="form-group">
                         <label for="message-text" class="col-form-label">Description:</label>
-                        <textarea class="form-control" id="message-text"></textarea>
+                        <textarea class="form-control" id={uniqueid + "message-text"}></textarea>
                     </div>
                     </form>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-primary">Confirm</button>
+                    <button type="button" class="btn btn-primary" id = {uniqueid+'confirmButton'}>Confirm</button>
                     <button type="button" class="btn btn-secondary"  id = {uniqueid+'closeButton'}>Close</button>
                 </div>
                 </div>
