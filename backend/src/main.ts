7268a97fc1c7c5c317a7abf08e462d8d3c858887
daniel-melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-11-08 10:16:53 -0500

    chore: dotenv

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
+import { config } from "dotenv";
 import express from "express";
 import { buildings } from "./routes/buildings.route";
 import { events } from "./routes/events.route";
 
+config();
+
 const app = express();
 app.use(express.json());
-app.use("/", express.static("../frontend/dist/"))
+app.use("/", express.static("../frontend/dist/"));
 
 const api = express.Router();
 api.use("/buildings", buildings);
@@ -12,7 +15,7 @@ api.use("/events", events);
 
 app.use("/api", api);
 
-const EXPRESS_PORT = 8000;
+const EXPRESS_PORT = process.env.PORT;
 app.listen(EXPRESS_PORT, () =>
   console.log("Server listening on port " + EXPRESS_PORT)
 );
