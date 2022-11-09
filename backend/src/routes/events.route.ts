0f2e884a5451def420d4506f1261c2cbab19ca0e
anthony <anthony_zalev@hotmail.com>
2022-11-08 20:30:25 -0500

    events api end point description

--- backend/src/routes/events.route.ts
+++ backend/src/routes/events.route.ts
@@ -4,6 +4,47 @@ import { urlBuilder } from "./util";
 
 export const events = Router();
 
+/*
+events:
+
+receive JSON OBJECT:
+
+{
+  name : <optional> string,
+  building : string,
+  room : string,
+  date: string "dd-mm-yyyy",
+  start_hr : integer 0 - 24,
+  end_hr : integer 0 - 24 (also greater than start_hr),
+  start_min: integer 0 - 60,
+  end_min: integer 0 - 60
+}
+
+
+return JSON OBJECT: 
+
+{
+  [
+    {
+      event: string :
+      {
+        name : <optional> string,
+        building : string,
+        room : string,
+        date: string "dd-mm-yyyy",
+        start_hr : integer 0 - 24,
+        end_hr : integer 0 - 24,
+        start_min: integer 0 - 60,
+        end_min: integer 0 - 60
+     }
+   },
+    ....
+  ]
+}
+
+
+
+*/
 events.get("/:id/", (req, res) => {
   const userURL = urlBuilder("users", req);
   const event = EVENT_LIST.find((e) => e.id === req.params.id);
