55288168c9f205202dafd23156ac218a888bd941
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-11-18 16:49:46 -0500

    feat: get building

--- backend/src/routes/buildings.route.ts
+++ backend/src/routes/buildings.route.ts
@@ -1,47 +1,33 @@
 import { Router } from "express";
-import { BUILDING_LIST, ROOM_LIST } from "../data/data";
-import { urlBuilder } from "./util";
+import prisma from "../db";
 
 export const buildings = Router();
 
 buildings.get("/", (req, res) => {
-  const buildingURL = urlBuilder("buildings", req);
-  const roomURL = urlBuilder("rooms", req);
-
-  res
-    .status(200)
-    .json(
-      BUILDING_LIST.map((b) => ({
-        ...b,
-        url: buildingURL(b.id),
-        rooms: ROOM_LIST.filter((r) => r.building_id === b.id).map((r) => ({
-          id: r.id,
-          number: r.number,
-          url: roomURL(r.id),
-        })),
-      }))
-    )
-    .end();
+  res.status(404).end();
 });
 
-buildings.get("/:id", (req, res) => {
-  const buildingURL = urlBuilder("buildings", req);
-  const roomURL = urlBuilder("rooms", req);
-  const b = BUILDING_LIST.find((b) => b.id === req.params.id);
-  if (!b) {
-    return res.status(404).end();
+buildings.get("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  if (!id) {
+    return res.status(400).end();
   }
 
-  res
-    .status(200)
-    .json({
-      ...b,
-      url: buildingURL(b.id),
-      rooms: ROOM_LIST.filter((r) => r.building_id === b.id).map((r) => ({
-        id: r.id,
-        url: roomURL(r.id),
-        number: r.number,
-      })),
-    })
-    .end();
+  try {
+    const building = await prisma.building.findFirst({
+      where: { id },
+    });
+
+    if (building) {
+      return res.status(200).json(building).end();
+    } else {
+      return res.status(404).end();
+    }
+  } catch (e) {
+    return res
+      .status(500)
+      .json({ errors: [e] })
+      .end();
+  }
 });
