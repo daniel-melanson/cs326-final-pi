d71bc6a26124b11ffdb6fe0c8dbceb7d4c238a82
daniel-melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-11-18 03:35:46 -0500

    fix: bootstrap > css

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,22 +3,24 @@ import Enact from "../Enact";
 
 export default function Home() {
   return (
-    <div id="home">
+    <div>
       <Navbar />
-      <main>
-        <h1 class="display-5 fw-bold">Campus Meet</h1>
-        <div class="col-6">
-          <p class="lead">Need a room? Campus Meet helps you find empty rooms on UMass' Campus.</p>
-          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <a type="button" class="btn btn-primary btn-lg px-4" href="/campus">
-              Find Rooms
-            </a>
-            <a type="button" class="btn btn-outline-secondary btn-lg px-4" href="/signup">
-              Sign Up
-            </a>
+      <div class="d-flex vh-100 justify-content-center">
+        <div class="d-flex justify-content-center align-items-center flex-column text-center">
+          <h1 class="display-5 fw-bold">Campus Meet</h1>
+          <div class="col-8">
+            <p class="lead">Need a room? Campus Meet helps you find empty rooms on UMass' Campus.</p>
+            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+              <a type="button" class="btn btn-primary btn-lg px-4" href="/campus">
+                Find Rooms
+              </a>
+              <a type="button" class="btn btn-outline-secondary btn-lg px-4" href="/signup">
+                Sign Up
+              </a>
+            </div>
           </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
