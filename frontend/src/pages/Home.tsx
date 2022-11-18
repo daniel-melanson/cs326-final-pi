909d8687566e664f896d98da16259566059e7db9
daniel-melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-11-18 02:23:54 -0500

    feat: home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,14 +3,14 @@ import Enact from "../Enact";
 
 export default function Home() {
   return (
-    <div>
+    <div id="home">
       <Navbar />
-      <div class="px-4 py-5 my-5 text-center">
+      <main>
         <h1 class="display-5 fw-bold">Campus Meet</h1>
-        <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4">Need a room? Campus Meet helps you find empty rooms on UMass' Campus.</p>
+        <div class="col-6">
+          <p class="lead">Need a room? Campus Meet helps you find empty rooms on UMass' Campus.</p>
           <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <a type="button" class="btn btn-primary btn-lg px-4 gap-3" href="/campus">
+            <a type="button" class="btn btn-primary btn-lg px-4" href="/campus">
               Find Rooms
             </a>
             <a type="button" class="btn btn-outline-secondary btn-lg px-4" href="/signup">
@@ -18,7 +18,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-      </div>
+      </main>
     </div>
   );
 }
