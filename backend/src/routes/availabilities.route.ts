447d93265531911aadc0ba9f12e880ff22ad213a
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-11-18 21:08:19 -0500

    feat: implement basic crud

--- backend/src/routes/availabilities.route.ts
+++ backend/src/routes/availabilities.route.ts
@@ -3,5 +3,5 @@ import { Router } from 'express';
 export const availabilities = Router();
 
 availabilities.get('/', (req, res) => {
-  res.status(404).end();
+  res.status(200).json([]).end();
 });
--- backend/src/routes/events.route.ts
+++ backend/src/routes/events.route.ts
@@ -1,11 +1,27 @@
 import { Router } from 'express';
+import prisma from '../db/index.js';
 
 export const events = Router();
 
-events.get('/:id/', (req, res) => {
-  res.status(404).end();
-});
+events.get('/:id', async (req, res) => {
+  const idString = req.params.id;
 
-events.post('/', (req, res) => {
-  res.status(404).end();
+  const id = Number(idString);
+  if (isNaN(id)) {
+    return res.status(400).end();
+  }
+
+  try {
+    const event = await prisma.event.findFirst({
+      where: { id },
+    });
+
+    if (event) {
+      return res.status(200).json(event).end();
+    } else {
+      return res.status(404).end();
+    }
+  } catch (e) {
+    return res.status(500).end();
+  }
 });
