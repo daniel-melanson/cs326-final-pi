009f05f6b51dd71a15dd5d3b55104cab0cc46821
daniel-melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-12-01 10:45:19 -0500

    feat: use handlers

--- backend/src/routes/buildings.route.ts
+++ backend/src/routes/buildings.route.ts
@@ -8,33 +8,7 @@ import { objectGETHandler } from './util.js';
 export const buildings = Router();
 
 buildings.get('/', async (req, res) => {
-  let buildings = await prisma.building.findMany()
-  res.status(200).json(buildings).end();
-});
-
-buildings.get('/:id', async (req, res) => {
-  const id = req.params.id;
-
-  if (!id) {
-    return res.status(400).end();
-  }
-
-  try {
-    const building = await prisma.building.findFirst({
-      where: { id: id},
-    });
-
-    if (building) {
-      return res.status(200).json(building).end();
-    } else {
-      return res.status(404).end();
-    }
-  } catch (e) {
-    return res.status(500).end();
-  }
-});
-
-  /*const buildings = await prisma.building.findMany({
+  const buildings = await prisma.building.findMany({
     include: {
       rooms: true,
     },
@@ -44,8 +18,8 @@ buildings.get('/:id', async (req, res) => {
       },
     ],
   });
-  res.send("Hey this is here")
-  //res.status(200).json(buildings.map((b) => serializeBuilding(req, b)));;
-  */
+
+  res.status(200).json(buildings.map((b) => serializeBuilding(req, b)));
+});
 
 buildings.get('/:id', ensureObjectId, injectObject('building', { rooms: true }), objectGETHandler('building'));
