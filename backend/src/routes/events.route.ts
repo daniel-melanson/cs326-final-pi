168ea0cbcead8a75d986118fe654f42cd2b212ef
daniel-melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-12-01 12:12:35 -0500

    chore: no user create event

--- backend/src/routes/events.route.ts
+++ backend/src/routes/events.route.ts
@@ -1,53 +1,8 @@
 import { Router } from 'express';
-import { body } from 'express-validator';
 import { ensureObjectId } from '../middleware/ensureObjectId.js';
 import { injectObject } from '../middleware/injectObject.js';
-import validate from '../middleware/validate.js';
 import { objectGETHandler } from './util.js';
 
 export const events = Router();
 
 events.get('/:id', ensureObjectId, injectObject('event', { room: true }), objectGETHandler('event'));
-
-// TODO check if user in authenticated
-events.post(
-  '/',
-  validate([
-    body('roomId').isString().isLength({ min: 5 }),
-    body('title').isString().isLength({ min: 5, max: 64 }),
-    body('startTime').isDate(),
-    body('endTime').isDate(),
-  ]),
-  async (req, res) => {
-    const { roomId, title, startTime, endTime } = req.body;
-
-    try {
-      // const event = await prisma.event.create({
-      //   data: {
-      //     room: {
-      //       connect: {
-      //         id: roomId,
-      //       },
-      //     },
-      //     title: title,
-      //     startTime: new Date(startTime),
-      //     endTime: new Date(endTime),
-      //     owner: {
-      //       connect: {
-      //         id: 0, // TODO: use authenticated user
-      //       },
-      //     },
-      //     organization: '', // TODO: Drop, make optional, or use defaults
-      //     type: '',
-      //     categories: '',
-      //     creationDate: new Date(),
-      //     state: '',
-      //   },
-      // });
-
-      return res.status(200).json({});
-    } catch (e) {
-      return res.status(500).json({ errors: [] });
-    }
-  },
-);
