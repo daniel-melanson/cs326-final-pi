08d28ac3ea5fb2975f1a605962f9b02ce0ea7cfa
daniel-melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-12-01 10:48:24 -0500

    chore: fix relation name

--- backend/src/routes/rooms.route.ts
+++ backend/src/routes/rooms.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { serializeEvent, serializeEventField } from '../db/serializers.js';
+import { serializeEventField } from '../db/serializers.js';
 import { ensureObjectId } from '../middleware/ensureObjectId.js';
 import { injectObject } from '../middleware/injectObject.js';
 import { objectGETHandler } from './util.js';
@@ -8,6 +8,6 @@ export const rooms = Router();
 
 rooms.get('/:id/', ensureObjectId, injectObject('room', { building: true }), objectGETHandler('room'));
 
-rooms.get('/:id/events', ensureObjectId, injectObject('room', { Event: true }), async (req, res) => {
-  res.status(200).json((req.room as any).Event.map((e: any) => serializeEventField(req, e)));
+rooms.get('/:id/events', ensureObjectId, injectObject('room', { events: true }), async (req, res) => {
+  res.status(200).json((req.room as any).events.map((e: any) => serializeEventField(req, e)));
 });
