0331356e09ab697b8f6ab9e56fae6333fec6da68
Anthony Zalev <anthony_zalev@hotmail.com>
2022-12-08 13:10:02 -0500

    added buttons and campus meet brings you to landing page now

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container-fluid justify-content-center">
-        <a className="navbar-brand" href="/campus">
+        <a className="navbar-brand" href="/">
           <i className="bi-image"></i> Campus Meet
         </a>
         <a className="navbar-brand mx-3" href="#">
